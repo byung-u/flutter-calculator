78609e6de3265e0c156d1037ddfc89b389a1351b
andre_jeon <andre.jeon@balancehero.com>
2019-08-23 14:25:20 +0900

    [fix] add calc buttons

--- lib/calculator-buttons.dart
+++ lib/calculator-buttons.dart
@@ -8,11 +8,13 @@ class CalculatorButtons extends StatelessWidget {
 
   final CalculatorButtonTapCallback onTap;
   final calculatorButtonRows = [
-    ['7', '8', '9', Calculations.DIVIDE],
-    ['4', '5', '6', Calculations.MULTIPLY],
-    ['1', '2', '3', Calculations.SUBTRACT],
-    [Calculations.PERIOD, '0', '00', Calculations.ADD],
-    [Calculations.CLEAR, Calculations.EQUAL]
+    ['6', '7', '8', '9', '10'],
+    ['1', '2', '3', '4', '5'],
+    ['0', '00', '000', '0000', '00000'],
+    [Calculations.ADD, Calculations.SUBTRACT, Calculations.MULTIPLY, Calculations.DIVIDE, Calculations.REMAIN],
+    [Calculations.PERIOD, Calculations.POWER, Calculations.REVERSE_INTEREST, Calculations.INTEREST, Calculations.DISCOUNT],
+    [Calculations.BITMUL, Calculations.BITDIV, Calculations.EQUAL, Calculations.EQUAL, Calculations.EQUAL],
+    [Calculations.CLEAR, Calculations.CLEAR, '', Calculations.EQUAL, Calculations.EQUAL]
   ];
 
   @override
